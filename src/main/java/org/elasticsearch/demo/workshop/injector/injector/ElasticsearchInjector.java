cc2434037c6f6edef8898ca241569c69920d98ba
David Pilato <david@pilato.fr>
2021-10-28 13:34:23 +0200

    Add the new Java Client

--- src/main/java/org/elasticsearch/demo/workshop/injector/injector/ElasticsearchInjector.java
+++ src/main/java/org/elasticsearch/demo/workshop/injector/injector/ElasticsearchInjector.java
@@ -19,6 +19,11 @@
 
 package org.elasticsearch.demo.workshop.injector.injector;
 
+import co.elastic.clients.base.RestClientTransport;
+import co.elastic.clients.base.Transport;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._core.InfoResponse;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -32,12 +37,11 @@ import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.Request;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.core.MainResponse;
-import org.elasticsearch.client.indices.PutIndexTemplateRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.demo.workshop.injector.bean.Person;
@@ -51,7 +55,9 @@ import java.util.concurrent.TimeUnit;
 public class ElasticsearchInjector extends Injector {
 
     private static final Logger logger = LogManager.getLogger(ElasticsearchInjector.class);
-    private final RestHighLevelClient client;
+    private final RestClient lowLevelClient;
+    private final RestHighLevelClient oldClient;
+    private final ElasticsearchClient client;
     private final BulkProcessor bulkProcessor;
     private final String index;
 
@@ -64,10 +70,22 @@ public class ElasticsearchInjector extends Injector {
                 .builder(HttpHost.create(host))
                 .setHttpClientConfigCallback(hcb -> hcb.setDefaultCredentialsProvider(credentialsProvider));
 
-        client = new RestHighLevelClient(builder);
+        // Create the old client
+        oldClient = new RestHighLevelClient(builder);
+
+        // Our low level client
+        // In the future, just use builder.build() instead of client.getLowLevelClient()
+        lowLevelClient = oldClient.getLowLevelClient();
+
+        // Create the new client (using the low level client)
+        Transport transport = new RestClientTransport(lowLevelClient, new JacksonJsonpMapper());
+        client = new ElasticsearchClient(transport);
+
+        // The bulk processor is only available in the old client for now.
+        // It will be added in 8.x
         bulkProcessor = BulkProcessor.builder(
-                (request, bulkListener) -> client.bulkAsync(request, RequestOptions.DEFAULT, bulkListener),
-                new SimpleLoggerListener())
+                (request, bulkListener) -> oldClient.bulkAsync(request, RequestOptions.DEFAULT, bulkListener),
+                new SimpleLoggerListener(), "persons")
                 .setBulkActions(bulkSize)
                 .setFlushInterval(TimeValue.timeValueSeconds(5))
                 .build();
@@ -76,15 +94,15 @@ public class ElasticsearchInjector extends Injector {
     @Override
     public void internalStart() throws IOException {
         // We check the cluster is running
-        MainResponse info = client.info(RequestOptions.DEFAULT);
-        logger.info("Injector connected to a node running elasticsearch {}", info.getVersion().toString());
+        InfoResponse info = client.info();
+        logger.info("Injector connected to a node running elasticsearch {}", info.version().number());
 
         // Create or Update the template
-        PutIndexTemplateRequest request = new PutIndexTemplateRequest(index);
         String schema = readFileInClasspath("/template.json");
-        request.source(schema, XContentType.JSON);
-        logger.info("Creating or updating template [{}]", index);
-        client.indices().putTemplate(request, RequestOptions.DEFAULT);
+        Request request = new Request("PUT", "/_template/" + index);
+        request.setJsonEntity(schema);
+        lowLevelClient.performRequest(request);
+        logger.info("Template [{}] created or updated", index);
     }
 
     @Override
@@ -112,9 +130,9 @@ public class ElasticsearchInjector extends Injector {
                 throw new RuntimeException("Can not close properly the bulk processor, e");
             }
         }
-        if (client != null) {
+        if (oldClient != null) {
             try {
-                client.close();
+                oldClient.close();
             } catch (IOException e) {
                 throw new RuntimeException("Can not close properly the elasticsearch Rest Client, e");
             }
