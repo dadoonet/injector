2cf263d525e622b6552d6269ac8af467272f124c
David Pilato <david@pilato.fr>
2021-10-28 10:24:21 +0200

    Rename scripts

---
+++ src/main/scripts/injector.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source .env
+
+time java -jar ${project.build.directory}/${project.build.finalName}.jar --debug
+
+#time java -jar ${project.build.directory}/${project.build.finalName}.jar --nb 1000000 --bulk 10000 --debug \
+#    --elasticsearch --es.host $ES_HOST --es.user elastic --es.pass $ES_PASS --es.index person \
+#    --appsearch --ap.host $AP_HOST --app.key $AP_KEY --ap.engine person
--- src/main/scripts/keys.sh
+++
@@ -1,4 +0,0 @@
-export AP_HOST="host-XYZ"
-export AP_KEY="private-XYZ"
-export ES_HOST="XYZ"
-export ES_PASS="XYZ"
