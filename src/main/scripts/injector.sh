b0d06fda5f3a9e1189578dc989d98646f6a5c837
David Pilato <david@pilato.fr>
2024-09-23 16:32:51 +0200

    Move to TestContainers

--- src/main/scripts/injector.sh
+++ src/main/scripts/injector.sh
@@ -4,5 +4,5 @@ source .env
 time java -jar ${project.build.directory}/${project.build.finalName}.jar --debug
 
 #time java -jar ${project.build.directory}/${project.build.finalName}.jar --nb 1000000 --bulk 10000 --debug \
-#    --elasticsearch --es.host $ES_HOST --es.user elastic --es.pass $ES_PASS --es.index person \
-#    --appsearch --ap.host $AP_HOST --app.key $AP_KEY --ap.engine person
+#    --elasticsearch --es.host $ES_HOST --es.user elastic --es.pass $ES_PASS --es.index person
+#    --console
--- src/test/java/fr/pilato/elasticsearch/injector/ElasticsearchInjectorIT.java
+++ src/test/java/fr/pilato/elasticsearch/injector/ElasticsearchInjectorIT.java
@@ -20,9 +20,10 @@
 package fr.pilato.elasticsearch.injector;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.ElasticsearchException;
+import co.elastic.clients.elasticsearch.core.InfoResponse;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import fr.pilato.elasticsearch.injector.bean.Person;
 import fr.pilato.elasticsearch.injector.runner.Generate;
@@ -30,72 +31,120 @@ import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.SSLContexts;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.elasticsearch.client.RestClient;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.utility.DockerImageName;
 
-import javax.net.ssl.SSLContext;
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.io.InputStream;
+import java.util.Properties;
 
+import static fr.pilato.elasticsearch.injector.SSLUtils.createContextFromCaCert;
+import static fr.pilato.elasticsearch.injector.SSLUtils.createTrustAllCertsContext;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class ElasticsearchInjectorIT {
-    private ElasticsearchClient client;
-    private ElasticsearchTransport transport;
+
+    private static final Logger logger = LogManager.getLogger();
+    private ElasticsearchClient client = null;
+    private static final String PASSWORD = "changeme";
+    private static ElasticsearchContainer container;
+    private static byte[] certAsBytes;
+
+    private static final String INDEX_NAME_DEFAULT = "person";
+    private static final String INDEX_NAME = "anotherindexname";
+
+    @BeforeAll
+    static void startTestContainers() throws IOException {
+        Properties props = new Properties();
+        props.load(ElasticsearchInjectorIT.class.getResourceAsStream("/version.properties"));
+        String version = props.getProperty("elasticsearch.version");
+        logger.info("Starting testcontainers with Elasticsearch {}.", version);
+        // Start the container. This step might take some time...
+        container = new ElasticsearchContainer(
+                DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch")
+                        .withTag(version))
+                .withPassword(PASSWORD)
+                .withReuse(true);
+        container.start();
+        certAsBytes = container.copyFileFromContainer(
+                "/usr/share/elasticsearch/config/certs/http_ca.crt",
+                InputStream::readAllBytes);
+    }
 
     @BeforeEach
-    void initClient() {
-        try {
-            SSLContextBuilder sslBuilder = SSLContexts.custom().loadTrustMaterial(null, (x509Certificates, s) -> true);
-            final SSLContext sslContext = sslBuilder.build();
+    void createClientAndCleanData() throws IOException {
+        client = getClient("https://" + container.getHttpHostAddress(), certAsBytes);
+        removeData(INDEX_NAME);
+        removeData(INDEX_NAME_DEFAULT);
+    }
 
-            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("elastic", "changeme"));
-            RestClient lowLevelClient = RestClient
-                    .builder(HttpHost.create("https://127.0.0.1:9200"))
-                    .setHttpClientConfigCallback(hcb -> hcb
-                            .setSSLContext(sslContext)
-                            .setSSLHostnameVerifier(new NoopHostnameVerifier())
-                            .setDefaultCredentialsProvider(credentialsProvider)
-                    )
-                    .build();
-
-            // Create the new client (using the low level client)
-            transport = new RestClientTransport(lowLevelClient, new JacksonJsonpMapper());
-            client = new ElasticsearchClient(transport);
-        } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
-            throw new RuntimeException(e);
+    @AfterEach
+    void stopElasticsearchClient() throws IOException {
+        if (client != null) {
+            client._transport().close();
         }
     }
 
     @Test
     void testInjector() throws IOException {
-        client.indices().delete(dir -> dir.index("person").ignoreUnavailable(true));
-        Generate.main(new String[]{"--elasticsearch", "--nb", "100"});
-        client.indices().refresh();
-        SearchResponse<Person> response = client.search(sr -> sr.index("person"), Person.class);
-        assertThat(response.hits().total().value(), is(100L));
-        // Check that the index template exists
-        assertThat(client.indices().getIndexTemplate(itb -> itb.name("person")).indexTemplates(), not(emptyIterable()));
+        testWithArgs(INDEX_NAME_DEFAULT, new String[]{
+                "--elasticsearch",
+                "--nb", "100",
+                "--es.host", "https://" + container.getHttpHostAddress()});
     }
 
-    @AfterEach
-    void stopClient() {
-        if (client != null) {
-            try {
-                transport.close();
-            } catch (IOException e) {
-                throw new RuntimeException("Can not close properly the elasticsearch Rest Client, e");
-            }
-        }
+    @Test
+    void testInjectorWithIndexName() throws IOException {
+        testWithArgs(INDEX_NAME, new String[]{
+                "--elasticsearch",
+                "--nb", "100",
+                "--es.host", "https://" + container.getHttpHostAddress(),
+                "--es.index", INDEX_NAME});
+    }
+
+    private void testWithArgs(String indexName, String[] args) throws IOException {
+        Generate.main(args);
+        client.indices().refresh();
+        SearchResponse<Person> response = client.search(sr -> sr.index(indexName), Person.class);
+        assertThat(response.hits().total().value(), is(100L));
+        // Check that the index template exists
+        assertThat(client.indices().getIndexTemplate(itb -> itb.name(indexName)).indexTemplates(), not(emptyIterable()));
+    }
+
+    private ElasticsearchClient getClient(String elasticsearchServiceAddress, byte[] certificate) throws IOException {
+        logger.debug("Trying to connect to {} {}.", elasticsearchServiceAddress,
+                certificate == null ? "with no ssl checks": "using the provided SSL certificate");
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", PASSWORD));
+
+        // Create the low-level client
+        RestClient restClient = RestClient.builder(HttpHost.create(elasticsearchServiceAddress))
+                .setHttpClientConfigCallback(hcb -> hcb
+                        .setDefaultCredentialsProvider(credentialsProvider)
+                        .setSSLContext(certificate != null ?
+                                createContextFromCaCert(certificate) : createTrustAllCertsContext())
+                ).build();
+
+        // Create the API client
+        ElasticsearchClient client = new ElasticsearchClient(new RestClientTransport(restClient, new JacksonJsonpMapper()));
+        InfoResponse info = client.info();
+        logger.debug("Client connected to a cluster running version {} at {}.", info.version().number(), elasticsearchServiceAddress);
+        return client;
+    }
+
+    private void removeData(String indexName) throws IOException {
+        logger.debug("Removing index [{}]", indexName);
+        client.indices().delete(dir -> dir.index(indexName).ignoreUnavailable(true));
+        try {
+            logger.debug("Removing index template data [{}]", indexName);
+            client.indices().deleteIndexTemplate(itb -> itb.name(indexName));
+        } catch (ElasticsearchException ignored) {}
     }
 }
